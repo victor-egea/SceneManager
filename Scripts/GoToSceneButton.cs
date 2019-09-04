31eb9ad9242854136fd5c984faf8d4a88a030721
victor-egea <51764922+victor-egea@users.noreply.github.com>
2019-09-04 10:19:07 -0700

    Test Scene Manager

---
+++ Scripts/GoToSceneButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using TGS.Lightning.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToSceneButton : MonoBehaviour
+{
+    [SerializeField] private string _SceneToLoad;
+    [SerializeField] private bool _IsPopup;
+    [SerializeField] private bool ClearHistory = false;
+
+    public void OnButtonClicked()
+    {
+        if (_IsPopup)
+        {
+            TGSSceneManager.Instance.OpenSceneAdditive(_SceneToLoad);
+        }
+        else
+        {
+            TGSSceneManager.Instance.GoToScene(_SceneToLoad, ClearHistory);
+        }
+    }
+}
---
+++ Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private Transform _RotateTransform;
+    [SerializeField] private Vector3 _RotateAxis;
+    [SerializeField] private float _RotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _RotateTransform.Rotate(_RotateAxis, _RotateSpeed);
+    }
+}
